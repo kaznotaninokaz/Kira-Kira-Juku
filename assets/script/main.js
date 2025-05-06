80bb1442cface9e12e17a56cc57dd7711cad6f11
kaznotaninokaz <kazzzzz0358@gmail.com>
2025-05-06 10:37:11 +0900

    BEMの記法に伴ってmainm変更

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,16 +1,16 @@
 // ハンバーガーメニュー
-var hamburger = $('.hamburger-menu');
+var hamburger = $('.hamburger__menu');
 // OPEN/CLOSEボタンをクリックしたら
-$('.hamburger-button').on('click', function () {
+$('.hamburger__button').on('click', function () {
   // console.log('click');
   // .hamburgerの表示・非表示を繰り返す
-  hamburger.toggleClass('hamburger-menu-active');
+  hamburger.toggleClass('hamburger__menu-active');
 });
 // 画面幅のサイズが変わったら
 $(window).on('resize', function () {
   // console.log('resize');
   // ハンバーガーメニューを閉じる
-  hamburger.removeClass('hamburger-menu-active');
+  hamburger.removeClass('hamburger__menu-active');
 });
 
 // タブ
